e7aca5a8048197c94ddd2097ad8a0b17bdad025c
IU14 <75305549+IU14@users.noreply.github.com>
2023-10-09 12:06:09 +0000

    working on react

--- my-app/src/index.js
+++ my-app/src/index.js
@@ -1,7 +1,7 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
 
-import RootComponent from './profile';
+import RootComponent from './product';
 
 const root = ReactDOM.createRoot(document.getElementById("root"));
 root.render(<RootComponent />);
