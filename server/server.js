51c70193d5448760212c4c0246830e0cbd90392a
IU14 <75305549+IU14@users.noreply.github.com>
2023-10-27 19:45:28 +0000

    working on my server - working on auto generated id

--- server/server.js
+++ server/server.js
@@ -1,36 +1,41 @@
 const express = require('express')
+const path = require('path')
+const cors = require('cors')
 const app = express()
 const port = 8000
-const path = require('path');
-//const cors = require('cors');
 
-//app.use(cors());
+app.use(express.json())
+app.use(cors())
 
-app.use(express.json());
 
 //gets HTML from the client directory
 app.get('/', (req, res) => {
     res.sendFile(path.join(__dirname,'../client/index.html') );
 })
 
-// sets up ITEM 
-let ITEM = [
+// sets up ITEM dictionary
+ITEM = {
+  1:
   {
-  "id": 0,
-  "user_id": "user1234", 
-  "keywords": [
-    "hammer",
-    "nails",
-    "tools"
-  ],
-  "description": "A hammer and nails set",
-  "image": "https://placekitten.com/200/300",
-  "lat": 51.2798438,
-  "lon": 1.0830275,
-  "date_from": "2023-10-22T17:38:31.805Z",
-  "date_to": "2023-10-22T17:38:31.805Z"
+    "id": 1,
+    "user_id": "user1234", 
+    "keywords": [
+      "hammer",
+      "nails",
+      "tools"
+    ],
+    "description": "A hammer and nails set",
+    "image": "https://placekitten.com/200/300",
+    "lat": 51.2798438,
+    "lon": 1.0830275,
+    "date_from": "2023-10-22T17:38:31.805Z",
+    "date_to": "2023-10-22T17:38:31.805Z"
   }
-]
+}
+
+app.getMaxListeners('/',  (req, res)=>
+{NEXT_ID = max(ITEM.keys()+1) })
+// max is not defined
 
 // returns item 
 app.get('/item', (req, res) => {
@@ -53,6 +58,7 @@ app.post('/item', (req, res) => {
 
 // delete item
 app.delete('/item/:id', (req, res) => {
+  // const id = parseFloat(req.params.id)
   ITEM = ITEM.filter((item)=> item.id!=req.params.id)
   res.status(204).json()
 })
@@ -63,8 +69,10 @@ app.listen(port, () => {
 
 /* to do:
 return item by item ID
-CORS (failed test)
-get the automated fields set up & working (failed test)
+get the automated fields set up & working (failed test) - item_id, dates 
 return HTML to the user (failed test)
-recording 52 
+image field is optional*
+time stamp - ISO standard date time
+
+recording 54
 */ 
