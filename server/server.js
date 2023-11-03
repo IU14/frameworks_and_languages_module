56b2f75d19ab33ec9a1c36a86093df1fa1bdc2d1
IU14 <75305549+IU14@users.noreply.github.com>
2023-11-03 12:58:28 +0000

    working on server - only 4 failing tests now

--- server/server.js
+++ server/server.js
@@ -15,6 +15,8 @@ const dateStamp = new Date();
 //gets HTML from the client directory
 app.get('/', (req, res) => {
     res.sendFile(path.join(__dirname,'../client/index.html') );
+
+  res.status(200)
 })
 
 // sets up ITEM 
@@ -38,51 +40,76 @@ ITEM = {
 };
 
 
-// returns item 
-app.get('/item', (req, res) => {
-  res.json(ITEM)
+// returns item by item id
+app.get('/item/:id', (req, res) => {
+  const id = req.params.id
+
+  if (!ITEM[id])
+  {
+    console.log("Item does not exist")
+    res.status(404).json()
+  }
+    
+  else 
+    console.log  (ITEM[id])
+    return res.status(200).json(ITEM[id])
 })
 
+// returns list
+app.get('/items', (req, res) => {
+  const list = Object.keys(ITEM).map((key) => [key, ITEM[key]]);
+
+  res.status(200).json(list)
+  console.log(list)
+})
 
 // posts items 
-app.post('/item', (req, res) => {
+app.post('/item', (req, res, next) => {
 
   const reqFields =["user_id","keywords","description","lat","lon"];
  
   if (!reqFields.every(field=> req.body.hasOwnProperty(field)))
   {
     console.log("Missing data")
-    return res.status (405).json()
+    return res.status(405).json()
   }
  
   const newITEM = {};
-  
-  ITEM[newITEM] = {
-      "id": Math.random(),
-      "user_id": req.body.user_id,
-      "keywords":req.body.keywords,
-      "description": req.body.description,
-      "image" : req.body.image,
-      "lat": req.body.lat,
-      "lon": req.body.lon,
-      "date_from" : dateStamp.toISOString(), 
-      "date_to": dateStamp.toISOString(),
+  const newID= Math.random();
+
+  for (let [key, value] of Object.entries(req.body)){
+    newITEM[key] = value;
+  }
+
+  ITEM[newID] = {
+    "id": newID,
+    "user_id": req.body.user_id,
+    "keywords":req.body.keywords,
+    "description": req.body.description,
+    "image" : req.body.image,
+    "lat": req.body.lat,
+    "lon": req.body.lon,
+    "date_from" : dateStamp.toISOString(), 
+    "date_to": dateStamp.toISOString(),
+
 },
-    res.status(201).json(ITEM)
+    res.status(201).json(ITEM[newID])
     console.log(ITEM)
 })
 
 
 // delete item
 app.delete('/item/:id', (req, res) => {
-  const id = parseFloat(req.params.id)
+  const id = req.params.id
 
-  if (id == req.params.id){
+  if (ITEM[id]){
     delete ITEM
     console.log("Item deleted") 
     return res.status(204).json()
-    
-  }
+    }
+  else 
+    console.log("No such item to delete")
+    res.status(404).json()
 })
 
 app.listen(port, () => {
@@ -90,8 +117,10 @@ app.listen(port, () => {
 })
 
 /* to do:
-return item by item ID
-return HTML to the user (failed test)
-ISSUE: when posting an object states its an [object object] and not the item number therefore getting a failure. 
+failed tests as of 3/11:
+Base endpoint should return html of some form to the user. 
+After POSTing an item, a GET to /items should have our new item as a last entry of the list/array
+date_from has been created and is a pauseable ISO datetime
+Create new_item and check that it appears in the items list
 recording 54
 */ 
